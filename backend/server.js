d0a5dff020905e86ce1ea4395d45729c4e7cc6d0
Pankaj <pankajbeniwal3112@gmail.com>
2024-03-09 16:05:28 +0530

    deploy to render

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,25 @@
 require("dotenv").config();
 const { Document } = require("./db");
-const io = require("socket.io");
 
-const server = require("http").createServer();
-
-const socketServer = io(server, {
+const io = require("socket.io")(process.env.PORT, {
   cors: {
     origin: process.env.CLIENT_URL,
     methods: ["GET", "POST"],
   },
 });
 
-socketServer.on("connection", (socket) => {
-  //   console.log("New user connected - ", socket.id);
+io.on("connection", (socket) => {
+//   console.log("New user connected - ", socket.id);
 
   socket.on("get-document", async (documentId) => {
     const document = await findOrCreateDocument(documentId);
-
+	
     socket.join(documentId);
     socket.emit("load-document", document.data);
 
     socket.on("send-changes", (delta) => {
       socket.broadcast.to(documentId).emit("receive-changes", delta);
-      console.log(delta);
+	  console.log(delta);
     });
 
     socket.on("save-document", async (data) => {
@@ -38,5 +35,3 @@ async function findOrCreateDocument(id) {
   if (document) return document;
   return await Document.create({ _id: id, data: "" });
 }
-
-export default server;
