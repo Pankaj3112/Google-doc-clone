ea593d41c98e9f2466313a4ad77d350d00814487
Pankaj <pankajbeniwal3112@gmail.com>
2024-03-16 17:19:56 +0530

    add .env

--- backend/server.js
+++ backend/server.js
@@ -12,19 +12,18 @@ const io = socketIo(server, {
   },
 });
 
-
 io.on("connection", (socket) => {
-//   console.log("New user connected - ", socket.id);
+  //   console.log("New user connected - ", socket.id);
 
   socket.on("get-document", async (documentId) => {
     const document = await findOrCreateDocument(documentId);
-	
+
     socket.join(documentId);
     socket.emit("load-document", document.data);
 
     socket.on("send-changes", (delta) => {
       socket.broadcast.to(documentId).emit("receive-changes", delta);
-	  console.log(delta);
+      console.log(delta);
     });
 
     socket.on("save-document", async (data) => {
@@ -40,7 +39,6 @@ async function findOrCreateDocument(id) {
   if (document) return document;
   return await Document.create({ _id: id, data: "" });
 }
-
 
 const PORT = process.env.PORT;
 server.listen(PORT, () => {
